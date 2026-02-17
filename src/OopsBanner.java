ab5e62fa389fe8bd1281f16b4b5817b1358ef6a9
fxmuru <hm5531@srmist.edu.in>
2026-02-17 12:55:38 +0530

    added to UC2 PrintOops

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,5 +1,14 @@
 public class OopsBanner{
     public static void main(String[] args){
-        System.out.println("OOPS");
+        String line1 = " *****   *****  ******* ******* ";
+        String line2 = "*     * *     * *     * *      ";
+        String line3 = "*     * *     * ******* *******";
+        String line4 = "*     * *     * *             *";
+        String line5 = " *****   *****  *       *******";
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
