9d51f2e26b8a3c6d3ccb4a9dfc1bdf30c8e48b1a
fxmuru <hm5531@srmist.edu.in>
2026-03-02 12:49:53 +0530

    UC6:Print OOPS

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,20 +1,48 @@
 public class OopsBanner {
-
     public static void main(String[] args) {
-
-        String[] bannerLines = {
-
-            String.join("", "  *****          *****          *******         ******* "),
-            String.join("", "*       *      *       *       *        *      *         "),
-            String.join("", "*       *      *       *       *        *      *         "),
-            String.join("", "*       *      *       *       *******          *******"),
-            String.join("", "*       *      *       *       *                        *"),
-            String.join("", "*       *      *       *       *                        *"),
-            String.join("", "  *****          *****         *                *******")
-        };
-
-        for (String line : bannerLines) {
-            System.out.println(line);
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    o[i] + "      " +
+                    o[i] + "      " +
+                    p[i] + "      " +
+                    s[i]
+            );
         }
+    }
+    public static String[] buildO() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+    }
+    public static String[] buildP() {
+        return new String[]{
+                "*******  ",
+                "*      * ",
+                "*      * ",
+                "*******  ",
+                "*        ",
+                "*        ",
+                "*        "
+        };
+    }
+    public static String[] buildS() {
+        return new String[]{
+                " ******* ",
+                "*        ",
+                "*        ",
+                " ******  ",
+                "       * ",
+                "       * ",
+                "*******  "
+        };
     }
 }
