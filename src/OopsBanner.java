44283ebb5b444e50e270a36d16b4120552849f43
fxmuru <hm5531@srmist.edu.in>
2026-03-02 12:39:02 +0530

    UC5:Print OOPS

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,9 +1,10 @@
 public class OopsBanner {
-
     public static void main(String[] args) {
-
-        String[] bannerLines = {
-
+        String[] bannerLines = createBanner();
+        printBanner(bannerLines);
+    }
+    public static String[] createBanner() {
+        return new String[] {
             String.join("", "  *****          *****          *******         ******* "),
             String.join("", "*       *      *       *       *        *      *         "),
             String.join("", "*       *      *       *       *        *      *         "),
@@ -12,7 +13,8 @@ public class OopsBanner {
             String.join("", "*       *      *       *       *                        *"),
             String.join("", "  *****          *****         *                *******")
         };
-
+    }
+    public static void printBanner(String[] bannerLines) {
         for (String line : bannerLines) {
             System.out.println(line);
         }
