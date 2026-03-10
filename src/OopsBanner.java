dd2d9dcdc9104d09027cc01cfd39f43c34a193cb
fxmuru <hm5531@srmist.edu.in>
2026-03-10 12:51:54 +0530

    UC8:Print OOPS

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,33 +1,13 @@
 import java.util.HashMap;
 import java.util.Map;
-
 public class OopsBanner {
-    static class CharacterPattern {
-        private char character;
-        private String[] pattern;
-        public CharacterPattern(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-        public char getCharacter() {
-            return character;
-        }
-        public String[] getPattern() {
-            return pattern;
-        }
-    }
-
     public static void main(String[] args) {
-
-        Map<Character, CharacterPattern> patternMap = initializePatterns();
-
-        printBanner("OOPS", patternMap);
+        Map<Character, String[]> patternMap = initializePatternMap();
+        renderBanner("OOPS", patternMap);
     }
-    public static Map<Character, CharacterPattern> initializePatterns() {
-
-        Map<Character, CharacterPattern> map = new HashMap<>();
-
-        map.put('O', new CharacterPattern('O', new String[]{
+    public static Map<Character, String[]> initializePatternMap() {
+        Map<Character, String[]> map = new HashMap<>();
+        map.put('O', new String[]{
                 "  *****  ",
                 " *     * ",
                 " *     * ",
@@ -35,9 +15,8 @@ public class OopsBanner {
                 " *     * ",
                 " *     * ",
                 "  *****  "
-        }));
-
-        map.put('P', new CharacterPattern('P', new String[]{
+        });
+        map.put('P', new String[]{
                 " ******* ",
                 " *     * ",
                 " *     * ",
@@ -45,9 +24,8 @@ public class OopsBanner {
                 " *       ",
                 " *       ",
                 " *       "
-        }));
-
-        map.put('S', new CharacterPattern('S', new String[]{
+        });
+        map.put('S', new String[]{
                 " ******* ",
                 " *       ",
                 " *       ",
@@ -55,17 +33,17 @@ public class OopsBanner {
                 "       * ",
                 "       * ",
                 " ******* "
-        }));
+        });
         return map;
     }
-    public static void printBanner(String text, Map<Character, CharacterPattern> map) {
+    public static void renderBanner(String text, Map<Character, String[]> map) {
         int height = 7;
         for (int row = 0; row < height; row++) {
             StringBuilder lineBuilder = new StringBuilder();
             for (char ch : text.toCharArray()) {
-                CharacterPattern cp = map.get(ch);
-                if (cp != null) {
-                    lineBuilder.append(cp.getPattern()[row]).append("   ");
+                String[] pattern = map.get(ch);
+                if (pattern != null) {
+                    lineBuilder.append(pattern[row]).append("   ");
                 }
             }
             System.out.println(lineBuilder.toString());
